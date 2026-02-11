c223b8d54f36279b1205357834ea27ba97cb83f8
BakhtMunirUet <bakhtmuniruet@gmail.com>
2026-02-11 18:05:26 +0100

    first commit

---
+++ src/main/java/com/example/checkoutsystems/CheckoutsystemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkoutsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutsystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutsystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/checkoutsystems/CheckoutsystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkoutsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutsystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
